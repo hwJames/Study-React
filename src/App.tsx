dfe99767f7f725c668e09f3956d47be5d4111e9f
KJH <kjhg5852@naver.com>
2021-03-18 13:47:32 +0900

    props 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Game(props: any) { 
+	return <h1>I love {props.fav}</h1>;
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World!
+    	<Game fav="LOL"/>
     </div>
   );
 }
-
 export default App;
